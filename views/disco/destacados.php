1387640e8090e9f99cf953940a93dc17b35e973e
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2020-04-15 11:50:30 +0200

    He modificado la vista de destacados.php

--- views/disco/destacados.php
+++ views/disco/destacados.php
@@ -1,38 +1,16 @@
 <h2 class="text-capitalize h2-responsive mx-auto"> DESTACADOS </h2>
 
 <div class="container-fluid mb-5 pb-5">
-    <div class="row">
+    <div class="row mx-auto">
         <?php while($disco = $discos->fetch_object()): ?>
-        <div class="col-12 col-sm-12.col-md-12 col-lg-3 col-xl-3 mb-4 w-25">
-            <!--Card-->
-            <div class="card card-cascade narrower mb-4" style="margin-top: 28px">
-
-                <!--Card image-->
-                <div class="view view-cascade">
-                    <img src="<?php echo($disco->imagen); ?>"
-                        class="card-img-top" alt="">
-                    <a>
-                        <div class="mask rgba-white-slight"></div>
-                    </a>
-                </div>
-                <!--/.Card image-->
-
-                <!--Card content-->
-                <div class="card-body card-body-cascade">
-                    <h5 class="pink-text"><i class="fas fa-utensils"></i> Culinary</h5>
-                    <!--Title-->
-                    <h4 class="card-title">Cheat day inspirations</h4>
-                    <!--Text-->
-                    <p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
-                        laboriosam, nisi ut
-                        aliquid ex ea commodi.</p>
-                    <a class="btn btn-unique">Button</a>
-                </div>
-                <!--/.Card content-->
-
-            </div>
-            <!--/.Card-->
-
+        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 border destacados text-center">
+            <img class="float-left" src="<?php echo($disco->imagen)?>" alt="<?php echo($disco->titulo)?>">
+            <span><?php echo($disco->titulo) ?></span>
+            <?php if(isset($_SESSION['identity'])): ?>
+            <a href="<?=base_url?>disco/album&id=<?php echo($disco->id) ?>" class="float-right">Ir</a>
+            <?php else: ?>
+                <i class="fas fa-info-circle float-right" data-toggle="tooltip" data-placement="right" title="Debe iniciar sesión o registrarse"></i>
+            <?php endif;?>
         </div>
         <?php endwhile; ?>
     </div>
