e18a3b3e42b3a6d0a2769c032b99c966abe8517c
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2020-04-15 11:51:51 +0200

    He añadido la function construct de la base de datos y una funcion para obtener todos los singles.

--- models/single.php
+++ models/single.php
@@ -6,7 +6,12 @@ class Single {
     public $id;
     public $disco_id;
     public $titulo;
+    private $db;
     #endregion
+
+    public function __construct(){
+        $this->db = Database::connect();
+    }
 
     #region Get y Set
 
@@ -74,6 +79,15 @@ class Single {
 
     #region Métodos
 
+    /**
+     * Obtener todos los singles de un disco según el id del disco_id
+     */
+    public function getSingles($id){
+
+        $album = $this->db->query("SELECT * FROM singles WHERE disco_id = '$id'");
+        return $album;
+
+    }
     
 
     #endregion
