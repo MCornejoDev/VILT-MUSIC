b782924931a4a314b6dbc66e873ddb5d051fd55f
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2020-04-16 21:58:01 +0200

    He añadido una etiqueta audio a la parte del footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,14 +1,24 @@
 </div>
 </div>
 <!-- Footer -->
+
 <!-- Footer -->
 <footer class="page-footer font-small blue fixed-bottom mt-5">
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">
-    <span class="text-white"> Miguel Ángel Cornejo Manzanares</span>
-  </div>
-  <!-- Copyright -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col sm-12 col-md-12 col-lg-12 col-xl-12 pt-2 pb-1 text-center">
+                <audio id="audio" controls>
+                    <source type="audio/mpeg">
+                    Your browser does not support the audio tag.
+                </audio>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">
+        <span class="text-white"> Miguel Ángel Cornejo Manzanares</span>
+    </div>
+    <!-- Copyright -->
 
 </footer>
 <!-- Footer -->
@@ -26,5 +36,6 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.1/js/mdb.min.js"></script>
 <!---Custom JS-->
 <script type="text/javascript" src="<?=base_url?>assets/js/registro.js"></script>
+<script type="text/javascript" src="<?=base_url?>assets/js/album.js"></script>
 
 </html>
