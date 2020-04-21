15eee70412c56ad633f0a3443769107aa35679a6
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2020-04-22 00:34:15 +0200

    He quitado varios console.log

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,13 +1,16 @@
+//Objeto de tipo Audio.
 let audio = new Audio();
 audio.volume = 0.3;
+//Variable que contendrá el tiempo en el que se ha pausado la canción en reproducción.
 let tiempo;
+//Variable que contendrá el id de la canción cuándo se haya pausado.
 let audio_id = -1;
 $(document).ready(function () {
 
   //Iniciar la reproducción de una canción
   $('.fa-play-circle').on('click', function () {
     let cancion = $(this).siblings('input:hidden');
-    console.log(cancion.attr('id'));
+
     if (audio_id == -1) {
       audio.src = cancion.val();
       audio_id = cancion.attr('id');
@@ -24,7 +27,6 @@ $(document).ready(function () {
         $(this).siblings('.fa-pause-circle').removeClass('hide')
       }
       else {
-        console.log("distintos");
         audio_id = cancion.attr('id');
         tiempo = 0;
         audio.currentTime = 0.0;
@@ -48,7 +50,6 @@ $(document).ready(function () {
     audio.pause();
     $(this).addClass('hide');
     $(this).siblings('.fa-play-circle').removeClass('hide')
-    console.log("Tiempo: " + tiempo + " y el audio_id es " + audio_id);
   });
 
   //Parar la reproducción de una canción
