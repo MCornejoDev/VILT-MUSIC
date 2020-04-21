371e107323f4d4518f0c37209b744c7dda4b4c5b
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2020-04-22 00:34:42 +0200

    He creado un nuevo parametro de base

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,6 @@
 <?php
 
 define("base_url","http://localhost/PHP/PROYECTO_PHP_SPOTIFY/");
+define("public_url","C:/laragon/www/PHP/PROYECTO_PHP_SPOTIFY/public/");
 define("controller_default","discoController");
 define("action_default","index");
