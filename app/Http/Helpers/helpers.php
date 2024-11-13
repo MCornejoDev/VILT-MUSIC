33de51626ddcd02e1794d28dd4892e85407c3b4f
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-11-13 10:30:16 +0100

    docs: New information about helper's functions added

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -2,6 +2,11 @@
 include __DIR__ . '/../../Models/Category.php';
 
 if (! function_exists('__')) {
+    /**
+     * This method prints the key from the language file
+     * @param string $key 
+     * @return void 
+     */
     function __(string $key): void
     {
         print(getKey($key));
@@ -9,6 +14,11 @@ if (! function_exists('__')) {
 }
 
 if (! function_exists('getKey')) {
+    /**
+     * This method gets the key from the language file
+     * @param string $key 
+     * @return string 
+     */
     function getKey(string $key): string
     {
         $splits = explode('.', $key);
@@ -30,6 +40,10 @@ if (! function_exists('getKey')) {
 }
 
 if (! function_exists('getLanguage')) {
+    /**
+     * This method returns a language
+     * @return string 
+     */
     function getLanguage(): string
     {
         return htmlspecialchars(empty($_GET['lang']) ? 'es' : $_GET['lang']);
@@ -37,6 +51,10 @@ if (! function_exists('getLanguage')) {
 }
 
 if (! function_exists('initSession')) {
+    /**
+     * This method inits the session
+     * @return void 
+     */
     function initSession(): void
     {
         if (session_status() === PHP_SESSION_NONE) {
@@ -48,6 +66,12 @@ if (! function_exists('initSession')) {
 }
 
 if (! function_exists('addToBag')) {
+    /**
+     * This method adds a data in a bag of session
+     * @param string $key 
+     * @param array $data 
+     * @return void 
+     */
     function addToBag(string $key, array $data): void
     {
         $_SESSION[$key] = $data;
@@ -55,6 +79,12 @@ if (! function_exists('addToBag')) {
 }
 
 if (! function_exists('existsKeyInBag')) {
+    /**
+     * This method checks if a key exists in a bag of session
+     * @param string $key 
+     * @param string $bag 
+     * @return bool 
+     */
     function existsKeyInBag(string $key, string $bag): bool
     {
         return array_key_exists($key, $_SESSION[$bag]);
@@ -62,6 +92,11 @@ if (! function_exists('existsKeyInBag')) {
 }
 
 if (! function_exists('hasValuesInBag')) {
+    /**
+     * This method checks if a bag has values
+     * @param string $bag 
+     * @return bool 
+     */
     function hasValuesInBag(string $bag): bool
     {
         return !empty($_SESSION[$bag]);
@@ -69,6 +104,10 @@ if (! function_exists('hasValuesInBag')) {
 }
 
 if (! function_exists('getCategories')) {
+    /**
+     * This method get all categories
+     * @return mysqli_result|bool 
+     */
     function getCategories(): mysqli_result|bool
     {
         return (new Category())->getAll();
@@ -76,6 +115,11 @@ if (! function_exists('getCategories')) {
 }
 
 if (!function_exists('redirectTo')) {
+    /**
+     * This method redirects to a url
+     * @param string $url 
+     * @return void 
+     */
     function redirectTo(string $url): void
     {
         print
@@ -88,6 +132,10 @@ if (!function_exists('redirectTo')) {
 }
 
 if (!function_exists('identityIsEmpty')) {
+    /**
+     * This method check if the identity is not empty
+     * @return bool 
+     */
     function identityIsEmpty(): bool
     {
         return empty($_SESSION['identity']);
@@ -95,6 +143,10 @@ if (!function_exists('identityIsEmpty')) {
 }
 
 if (!function_exists('isAdmin')) {
+    /**
+     * This method checks if the role's identity is a admin
+     * @return bool 
+     */
     function isAdmin(): bool
     {
         return isset($_SESSION['identity']['role']) && $_SESSION['identity']['role'];
@@ -102,8 +154,34 @@ if (!function_exists('isAdmin')) {
 }
 
 if (!function_exists('makeDirectory')) {
+    /**
+     * This method creates a folder if the directory doesn't exist
+     * @param string $folder 
+     * @return bool 
+     */
     function makeDirectory(string $folder): bool
     {
         return is_dir($folder) || mkdir($folder, 0755, true);
+    }
+}
+
+if (!function_exists('getData')) {
+    /**
+     * This method returns the data from the request.
+     * @return array 
+     */
+    function getData(): array
+    {
+        $data = [];
+
+        foreach ($_POST as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        foreach ($_FILES as $key => $value) {
+            $data[$key] = $value;
+        }
+
+        return $data;
     }
 }
